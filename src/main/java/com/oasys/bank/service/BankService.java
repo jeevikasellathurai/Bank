41dc4619112093bc4cb16a15a10abbe711f89176
jeevikasellathurai <jeevikasr@gmail.com>
2023-12-23 16:03:45 +0530

    initial commit

--- src/main/java/com/oasys/bank/service/BankService.java
+++ src/main/java/com/oasys/bank/service/BankService.java
@@ -1,7 +1,9 @@
-package com.oasys.bank.dao;
+package com.oasys.bank.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.oasys.bank.dao.BankDao;
 
 @Service
 public class BankService {
